000b8e28bd15249c5b935a4836a92297576452b8
TideS <tidesmain@gmail.com>
2024-05-03 07:36:46 +0500

    Whatever

---
+++ Demo/Asset/Shader/Shared.glsl
@@ -0,0 +1,21 @@
+#extension GL_EXT_buffer_reference : require
+
+#define RR_UBER3D_SET_GLOBALS 0
+
+struct Vertex {
+    vec3 position;
+    float uvX;
+    vec3 normal;
+    float uvY;
+    vec4 color;
+};
+
+layout(buffer_reference, std430) readonly buffer VertexBuffer {
+    Vertex vertices[];
+};
+
+vec3 processNormal(vec3 normal, mat4 model)
+{
+    mat3 normalMatrix = mat3(transpose(inverse(model)));
+    return normalMatrix * normal;
+}
