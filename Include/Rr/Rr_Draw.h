59f70a0aca2ca9f958cd0b318d373a4cf242413d
TideS <tidesmain@gmail.com>
2024-05-17 11:56:14 +0500

    Whatever

--- Include/Rr/Rr_Draw.h
+++ Include/Rr/Rr_Draw.h
@@ -1,7 +1,5 @@
 #pragma once
 
-typedef struct Rr_RenderingContext Rr_RenderingContext;
-
 #include "Rr_App.h"
 #include "Rr_String.h"
 #include "Rr_Text.h"
@@ -9,6 +7,13 @@ typedef struct Rr_RenderingContext Rr_RenderingContext;
 #include "Rr_Material.h"
 #include "Rr_Image.h"
 #include "Rr_Mesh.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct Rr_RenderingContext Rr_RenderingContext;
 
 typedef enum Rr_DrawTextFlags
 {
@@ -24,11 +29,6 @@ typedef struct Rr_BeginRenderingInfo
     Rr_Image* AdditionalAttachment;
     const void* GlobalsData;
 } Rr_BeginRenderingInfo;
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
 
 Rr_RenderingContext* Rr_BeginRendering(Rr_App* App, Rr_BeginRenderingInfo* Info);
 
