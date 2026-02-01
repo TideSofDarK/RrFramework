159b02a8cfcb1c94169945bc268fd0068b119e85
TideS <tidesmain@gmail.com>
2026-02-01 10:08:01 +0500

    Whatever

---
+++ Examples/Readback/Readback.comp.glsl
@@ -0,0 +1,11 @@
+#version 460 core
+
+layout(local_size_x = 32, local_size_y = 32) in;
+
+layout(rgba8, set = 0, binding = 0) uniform image2D Image;
+
+void main() {
+    ivec2 Coord = ivec2(gl_GlobalInvocationID.xy);
+    vec3 Color = vec3(vec2(Coord) / vec2(256.0), 0.0);
+    imageStore(Image, Coord, vec4(Color, 1.0));
+}
--- Examples/Readback/Readback.cxx
+++ Examples/Readback/Readback.cxx
@@ -1,3 +1,5 @@
+#include "ExampleAssets.inc"
+
 #include <Rr/Rr.h>
 
 #include <cassert>
@@ -5,15 +7,39 @@
 
 struct SReadbackApp
 {
+    Rr_PipelineLayout *PipelineLayout{};
+    Rr_ComputePipeline *ComputePipeline{};
     Rr_Image2D *ColorImage{};
     Rr_Buffer *ReadbackBuffer{};
 
     void Init()
     {
+        std::array Bindings = {
+            Rr_Binding{
+                .Index = 0,
+                .Type = RR_BINDING_TYPE_STORAGE_IMAGE,
+                .Stages = RR_SHADER_STAGE_COMPUTE_BIT,
+            },
+        };
+        std::array BindingSets = {
+            Rr_BindingSet{ Bindings.size(), Bindings.data() },
+        };
+        PipelineLayout =
+            Rr_CreatePipelineLayout(BindingSets.size(), BindingSets.data());
+
+        Rr_Asset ComputeShader = Rr_LoadAsset(EXAMPLE_ASSET_READBACK_COMP_SPV);
+        Rr_ShaderInfo ShaderInfo = {
+            .SPVSize = ComputeShader.Size,
+            .SPVData = ComputeShader.Pointer,
+        };
+
+        ComputePipeline = Rr_CreateComputePipeline(&ShaderInfo, PipelineLayout);
+
         ColorImage = Rr_CreateImage2D(
             { 256, 256 },
             RR_IMAGE_FORMAT_R8G8B8A8_UNORM,
-            RR_IMAGE_FLAGS_COLOR_ATTACHMENT_BIT | RR_IMAGE_FLAGS_TRANSFER_BIT);
+            RR_IMAGE_FLAGS_STORAGE_BIT | RR_IMAGE_FLAGS_TRANSFER_BIT);
+
         ReadbackBuffer = Rr_CreateBuffer(
             256 * 256 * 4,
             RR_BUFFER_FLAGS_MAPPED_BIT | RR_BUFFER_FLAGS_READBACK_BIT);
@@ -23,13 +49,11 @@ struct SReadbackApp
     {
         Rr_Graph *SubGraph = Rr_BeginGraph(RR_QUEUE_TYPE_MAIN);
 
-        float Channel = static_cast<float>(Rr_GetTimeSeconds());
-        Channel = Channel - static_cast<long>(Channel);
+        Rr_GraphNode *ComputeNode = Rr_AddComputeNode(SubGraph);
+        Rr_BindComputePipeline(ComputeNode, ComputePipeline);
+        Rr_BindStorageImage2DRW(ComputeNode, ColorImage, 0, 0);
+        Rr_Dispatch(ComputeNode, 256 / 32, 256 / 32, 1);
 
-        Rr_ClearColorImage2D(
-            SubGraph,
-            { .Vec4 = { Channel, Channel, Channel, 1.0f, } },
-            ColorImage);
         Rr_CopyImage2DToBuffer(
             SubGraph,
             ColorImage,
@@ -39,14 +63,18 @@ struct SReadbackApp
             0,
             ReadbackBuffer,
             0);
+
         Rr_EndGraph(SubGraph);
+
+        Rr_Image2D *SwapchainImage = Rr_GetSwapchainImage();
+        Rr_IntVec2 SwapchainExtent = Rr_GetImage2DExtent(SwapchainImage);
 
         Rr_BlitImage2D(
             Rr_GetGraph(),
             ColorImage,
             Rr_GetSwapchainImage(),
             { 0, 0, 256, 256 },
-            { 0, 0, 256, 256 },
+            { 0, 0, SwapchainExtent.X, SwapchainExtent.Y },
             RR_IMAGE_ASPECT_COLOR_BIT);
 
         uint8_t *BufferData =
@@ -54,17 +82,41 @@ struct SReadbackApp
 
         Rr_UIBeginWindow("Readback.cxx");
         {
-            Rr_IntVec4 Value = { BufferData[0],
-                                 BufferData[1],
-                                 BufferData[2],
-                                 BufferData[3] };
-            Rr_UIInputInt4("Pixel Value", Value.Elements);
+            Rr_UIText(
+                "This examples demonstrates copying an image into a\n"
+                "CPU-visible buffer and reading pixel color under the cursor.");
+
+            Rr_Vec2 MousePosition = Rr_GetMousePosition();
+            Rr_Vec2 SwapchainExtentF = {
+                static_cast<float>(SwapchainExtent.Width),
+                static_cast<float>(SwapchainExtent.Height)
+            };
+            Rr_Vec2 MouseNormalized = MousePosition / SwapchainExtentF;
+            MouseNormalized = Rr_MinV2(
+                Rr_MaxV2(MouseNormalized, Rr_V2F(0.0f)),
+                SwapchainExtentF);
+            Rr_Vec2 PixelCoordF = MouseNormalized * 255.0f;
+            Rr_IntVec2 PixelCoord = {
+                static_cast<int>(PixelCoordF.X),
+                static_cast<int>(PixelCoordF.Y),
+            };
+            uint8_t *PixelChannels =
+                BufferData + PixelCoord.Y * 256 * 4 + PixelCoord.X * 4;
+            Rr_Vec3 PixelColor = {
+                static_cast<float>(PixelChannels[0]) / 255.0f,
+                static_cast<float>(PixelChannels[1]) / 255.0f,
+                static_cast<float>(PixelChannels[2]) / 255.0f,
+            };
+
+            Rr_UIInputColor3("Pixel Color", PixelColor.Elements);
         }
         Rr_UIEndWindow();
     }
 
     void Cleanup()
     {
+        Rr_ReleasePipelineLayout(PipelineLayout);
+        Rr_ReleaseComputePipeline(ComputePipeline);
         Rr_ReleaseImage(ColorImage);
         Rr_ReleaseBuffer(ReadbackBuffer);
     }
