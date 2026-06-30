84b4b5083d02d14c0526233be91d2a1f67008f6b
TideS <tidesmain@gmail.com>
2026-06-30 05:50:49 +0500

    Whatever

--- Source/Rr_UI.c
+++ Source/Rr_UI.c
@@ -22,14 +22,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
+#include "Rr_UI.h"
+
+#include "Rr_BuiltinAssets.inc"
+
 #ifdef RR_GNU_OR_CLANG
 #pragma GCC diagnostic ignored "-Wmissing-format-attribute"
 #pragma GCC diagnostic ignored "-Wformat-nonliteral"
 #endif
-
-#include "Rr_UI.h"
-
-#include "Rr_BuiltinAssets.inc"
 
 #define RR_LOG_MACRO_CATEGORY RR_LOG_CATEGORY_UI
 #include "Rr_LogMacro.h"
